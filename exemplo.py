0cddab7f071e3e646da2e7d3dcc55d329ea9e4dc
Wilton Sales <saleswilton@hotmail.com>
2025-04-11 20:20:47 -0300

    Função soma aceita vários parâmetros

--- exemplo.py
+++ exemplo.py
@@ -1,6 +1,12 @@
-def soma (num1, num2):
-    return num1 + num2
+def soma (*args):
+    return sum(args)
 
 if __name__ == "__main__":
     print(soma(2, 3))
+    print(soma(2, 5, 10))
+    print(soma(3, 5, 6, 90))
+
+
+
+
 
