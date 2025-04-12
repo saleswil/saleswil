2a9cd6d7e4135294fb10ac560444c3e793448ed8
Wilton Sales <saleswilton@hotmail.com>
2025-04-12 18:22:31 -0300

    Primeira versão do notebook

---
+++ 01_exemplo_jupytext.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+#   kernelspec:
+#     display_name: projeto_novo
+#     language: python
+#     name: projeto_novo
+# ---
+
+# %%
+2 + 2
+
+# %%
